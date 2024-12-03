d06d3378becb7fc1bb356976245e7de20b447321
SiddharajYadav <sidharajyadav157@gmail.com>
2024-12-03 09:25:45 +0530

    Update Collection.tsx

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -12,7 +12,7 @@ type CollectionProps = {
   totalPages?: number,
   urlParamName?: string,
   collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
-}
+} 
 
 const Collection = ({
   data,
