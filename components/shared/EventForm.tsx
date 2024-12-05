30dea1faee3c1a3855bf152644c70e83fa1ecec6
SiddharajYadav <sidharajyadav157@gmail.com>
2024-12-05 13:41:19 +0530

    Update EventForm.tsx

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod" 
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
