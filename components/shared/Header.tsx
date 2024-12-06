1f36eec99cf9b01803d4cd7defd72cd89f5ebb38
SiddharajYadav <sidharajyadav157@gmail.com>
2024-12-06 13:06:09 +0530

    Update Header.tsx

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
 import NavItems from "./NavItems"
-import MobileNav from "./MobileNav"
+import MobileNav from "./MobileNav" 
 
 const Header = () => {
   return (
